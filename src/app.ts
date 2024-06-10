7bd51fa4339fa4882c93ae823e8c5e2c87b9c5ea
jackflaggg <rasul.khamzinnn@gmail.com>
2024-06-10 14:34:41 +0500

    add TESTING DATA

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import {SETTINGS} from "./settings";
 import {blogsRouter} from "./features/blogs";
+import {testingRouter} from "./features/testing";
+import {postRouter} from "./features/posts";
 
 export const app = express();
 app.use(express.json())
 app.use(cors())
 
 app.use(SETTINGS.PATH.BLOGS, blogsRouter);
+app.use(SETTINGS.PATH.POSTS, postRouter);
+app.use(SETTINGS.PATH.TESTING, testingRouter);
