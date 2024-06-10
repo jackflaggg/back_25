87c3cf54f9639b7f564fe0be2b0b94da927bd25b
jackflaggg <rasul.khamzinnn@gmail.com>
2024-06-11 01:30:26 +0500

    add vercel

--- src/repositories/testing-repositories.ts
+++
@@ -1,9 +0,0 @@
-import {db} from "../db/db";
-
-export const testingRepositories = {
-    delAllRepositories(){
-        db.posts.length = 0;
-        db.blogs.length = 0;
-        return;
-    }
-}
