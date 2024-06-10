dc0ca84996ed1eb582f173fa01d79c412d3c1372
jackflaggg <rasul.khamzinnn@gmail.com>
2024-06-11 01:27:36 +0500

    add vercel

--- src/features/testing/index.ts
+++ src/features/testing/index.ts
@@ -1,7 +1,10 @@
 import {Router} from "express";
-import {SETTINGS} from "../../settings";
-import {deleteAllData} from "./controllers/deleteAllData";
+import {db} from "../../db/db";
 
 export const testingRouter = Router();
 
-testingRouter.delete(SETTINGS.PATH.TESTING, deleteAllData)
+testingRouter.delete('/all-data', (req, res) => {
+    db.posts.length = 0;
+    db.blogs.length = 0;
+    res.status(204).json({})
+})
