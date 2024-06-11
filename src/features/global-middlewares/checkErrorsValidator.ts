1f95a6b3d82527feb69dfa4a5c239351b57ec0f5
jackflaggg <rasul.khamzinnn@gmail.com>
2024-06-11 15:28:14 +0500

    add vercel

--- src/features/global-middlewares/checkErrorsValidator.ts
+++ src/features/global-middlewares/checkErrorsValidator.ts
@@ -4,16 +4,17 @@ import {HTTP_STATUSES} from "../../types/types";
 import {FieldNamesType} from "../../input-output-types/output-errors-type";
 
 export const inputCheckErrorsMiddleware = (req: Request, res: Response, next: NextFunction): void => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()){
-        const uniqueErrors = new Map<string, {message: string, field: string}>();
-        errors.array({ onlyFirstError: true }).map(err => {
-            if (!uniqueErrors.has(err.type)){
-                uniqueErrors.set(err.type, {message: err.msg, field: err.type});
-            }
-        });
-        res.status(HTTP_STATUSES.BAD_REQUEST_400).send(uniqueErrors);
-        return;
+    const e = validationResult(req)
+    if (!e.isEmpty()) {
+        const eArray = e.array({onlyFirstError: true}) as { path: FieldNamesType, msg: string }[]
+        // console.log(eArray)
+
+        res
+            .status(400)
+            .json({
+                errorsMessages: eArray.map(x => ({ message: x.msg, field: x.path}))
+            })
+        return
     }
     next()
 }
