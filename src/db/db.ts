3165e403ec40c5400f3ecb8cc9ae76a124ba4686
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-02 22:44:57 +0500

    add mongo URL

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,7 +5,7 @@ import {SETTINGS} from "../settings";
 
 dotenv.config()
 
-const mongoURI = process.env.MONGO_URL // 'mongodb+srv://jackflagg:230900@clustersprint3.g4fqcoz.mongodb.net?retryWrites=true&w=majority' //&appName=ClusterSprint3'
+const mongoURI = process.env.MONGO_URL || 'mongodb+srv://jackflagg:230900@clustersprint3.g4fqcoz.mongodb.net?retryWrites=true&w=majority&appName=ClusterSprint3'
 const client: MongoClient = new MongoClient(mongoURI!);
 
 export const database: Db = client.db(SETTINGS.DB_NAME);
