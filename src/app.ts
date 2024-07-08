24ec2393be3885f0fba69399b738e930f41d1996
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-08 19:14:02 +0500

    add error

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,10 @@ import {testingRouter} from "./routes/testing/testing-routes";
 import {vercelRouter} from "./routes/vercel/vercel-router";
 
 export const app = express();
-app.use(express.json())
-app.use(cors())
-app.use(SETTINGS.PATH.VERCEL, vercelRouter)
-app.use(SETTINGS.PATH.TESTING, testingRouter);
+app.use(express.json());
+app.use(cors());
 
+app.use(SETTINGS.PATH.VERCEL, vercelRouter);
+app.use(SETTINGS.PATH.TESTING, testingRouter);
 app.use(SETTINGS.PATH.BLOGS, blogsRouter);
 app.use(SETTINGS.PATH.POSTS, postRouter);
