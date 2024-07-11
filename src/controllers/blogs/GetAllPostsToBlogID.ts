9e450789f6dae95570672521afbb3d59ffdaf411
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-11 17:31:33 +0500

    add query

--- src/controllers/blogs/GetAllPostsToBlogID.ts
+++ src/controllers/blogs/GetAllPostsToBlogID.ts
@@ -1,13 +1,9 @@
 import {BlogParamsModel, HTTP_STATUSES, RequestWithParamsAndQuery} from "../../models/common-types";
 import {Response} from "express";
 import {QueryBlogInputModels} from "../../models/blog/input/get-query.blog.input.models";
-import {postMapper} from "../../models/post/mapper/post-mapper";
 import {helperToPost} from "../../middlewares/helper-query-get";
 import {ObjectId} from "mongodb";
 import {blogsQueryRepositories} from "../../repositories/blogs-query-repository";
-import {postsQueryRepository} from "../../repositories/posts-query-repository";
-import {QueryPostInputModels} from "../../models/post/input/get-query.post.input.models";
-import {blogsService} from "../../domain/blog/blog-service";
 
 export const getAllPostsToBlogID = async (req: RequestWithParamsAndQuery<BlogParamsModel, QueryBlogInputModels>, res: Response<any>) => {
     const blogId = req.params.id;
