f1a7f6ce3a0b714a4dd88aa540a7d1ded5ed5a4d
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-13 01:17:37 +0500

    add refactor

--- src/repositories/blogs-query-repository.ts
+++ src/repositories/blogs-query-repository.ts
@@ -1,7 +1,6 @@
 import {blogsCollections, postsCollections} from "../db/db";
 import {blogMapper} from "../models/blog/mapper/blog-mapper";
 import {QueryBlogInputModels} from "../models/blog/input/get-query.blog.input.models";
-import {helperToBlog, helperToPost} from "../middlewares/helper-query-get";
 import {postMapper} from "../models/post/mapper/post-mapper";
 import {OutputBlogModel} from "../models/blog/output/blog.output.models";
 import {ObjectId} from "mongodb";
@@ -9,23 +8,21 @@ import {ObjectId} from "mongodb";
 
 export const blogsQueryRepositories = {
     async getAllBlog(queryParamsToBlog: QueryBlogInputModels): Promise<any> {
-        const { pageNumber, pageSize, sortBy, sortDirection, searchNameTerm} = helperToBlog(queryParamsToBlog || {}/*queryParams*/);
-
         const blogs = await blogsCollections
-            .find(searchNameTerm ? {name: {$regex: searchNameTerm, $options: 'i'}} : {})
-            .sort(sortBy, sortDirection)
-            .skip((Number(pageNumber) - 1) * Number(pageSize))
-            .limit(Number(pageSize))
+            .find(queryParamsToBlog.searchNameTerm ? {name: {$regex: queryParamsToBlog.searchNameTerm, $options: 'i'}} : {})
+            .sort(queryParamsToBlog.sortBy, queryParamsToBlog.sortDirection)
+            .skip((Number(queryParamsToBlog.pageNumber) - 1) * Number(queryParamsToBlog.pageSize))
+            .limit(Number(queryParamsToBlog.pageSize))
             .toArray();
 
-        const totalCountBlogs = await blogsCollections.countDocuments(searchNameTerm ? { name: { $regex: searchNameTerm, $options: "i" }} : {});
+        const totalCountBlogs = await blogsCollections.countDocuments(queryParamsToBlog.searchNameTerm ? { name: { $regex: queryParamsToBlog.searchNameTerm, $options: "i" }} : {});
 
-        const pagesCount = Math.ceil(totalCountBlogs / Number(pageSize));
+        const pagesCount = Math.ceil(totalCountBlogs / Number(queryParamsToBlog.pageSize));
 
         return {
             pagesCount: +pagesCount,
-            page: +pageNumber,
-            pageSize: +pageSize,
+            page: +queryParamsToBlog.pageNumber,
+            pageSize: +queryParamsToBlog.pageSize,
             totalCount: +totalCountBlogs,
             items: blogs.map(blog => blogMapper(blog)),
         };
@@ -39,8 +36,6 @@ export const blogsQueryRepositories = {
         return blogMapper(blog)
     },
     async getPostsToBlogID(paramsToBlogID: string, queryParamsPosts?: any): Promise<any> {
-        //const { pageNumber, pageSize, sortBy, sortDirection} = helperToPost(queryParamsPosts || {}/*queryParams*/);
-
         const posts = await postsCollections
             .find({blogId: paramsToBlogID})
             .sort(queryParamsPosts.sortBy, queryParamsPosts.sortDirection)
