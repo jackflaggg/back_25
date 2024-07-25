42bfd27dda86259cfc1964c9a2356840ed9a9bb5
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-25 15:31:36 +0500

    add create router

--- src/controllers/users/CreateUser.ts
+++ src/controllers/users/CreateUser.ts
@@ -1,3 +1,20 @@
-export const createUserController = async (req: Request, res: Response) => {
+import {HTTP_STATUSES, RequestWithBody, ResponseBody} from "../../models/common-types";
+import {InputCreateBlogModel} from "../../models/blog/input/create.blog.input.models";
+import {OutputBlogModel} from "../../models/blog/output/blog.output.models";
+import {blogsService} from "../../domain/blog/blog-service";
+import {blogsQueryRepositories} from "../../repositories/blogs/blogs-query-repository";
+import {userService} from "../../domain/user/user-service";
+import {usersQueryRepository} from "../../repositories/users/users-query-repository";
 
+export const createUserController = async (req: RequestWithBody<any>,
+                                           res:ResponseBody<any>) => {
+    const createdUserId = await userService.createUser(req.body);
+
+    const user = await usersQueryRepository.getUserById(createdUserId!.id)
+
+    if (!user) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+        return
+    }
+    res.status(HTTP_STATUSES.CREATED_201).send(user);
 }
