6a9e745626601fbf1ad240bc9712c6b0553abc36
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-25 14:41:54 +0500

    add delete router

--- src/repositories/users/users-db-repository.ts
+++ src/repositories/users/users-db-repository.ts
@@ -1,8 +1,12 @@
+import {usersCollection} from "../../db/db";
+import {ObjectId} from "mongodb";
+
 export const UsersDbRepository = {
     async createUser(): Promise<any> {
 
     },
     async deleteUser(id: string): Promise<any> {
-
+        const deleteUser = await usersCollection.deleteOne({_id: new ObjectId(id)});
+        return deleteUser.acknowledged;
     }
 }
