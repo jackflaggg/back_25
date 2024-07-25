bab5fc096c3d4cadb968bdb28c63b844171f1ee7
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-25 23:10:30 +0500

    add auth validator

---
+++ src/validators/authValidator.ts
@@ -0,0 +1,39 @@
+import {body} from "express-validator";
+import {inputCheckErrorsMiddleware} from "../utils/middlewares/checkErrorsValidator";
+
+export const loginValidator  = body('login')
+    .isString()
+    .withMessage('this is not string')
+    .trim()
+    .notEmpty()
+    .withMessage('empty')
+    .isLength({min: 3, max: 10})
+    .withMessage('more then 10 or min 3')
+    .matches(/^[a-zA-Z0-9_-]*$/)
+    .withMessage('login length should be from 3 to 10 symbols');
+
+export const passwordValidator  = body('password')
+    .isString()
+    .withMessage('this is not string')
+    .trim()
+    .notEmpty()
+    .withMessage('empty')
+    .isLength({min: 6, max: 20})
+    .withMessage('password length should be from 6 to 20 symbols');
+
+export const emailValidator  = body('email')
+    .isString()
+    .withMessage('this is not string')
+    .trim()
+    .notEmpty()
+    .withMessage('empty')
+    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    .withMessage('email should follow the pattern: example@example.com');
+
+
+export const authCreateValidator = [
+    loginValidator,
+    passwordValidator,
+    emailValidator,
+    inputCheckErrorsMiddleware
+]
