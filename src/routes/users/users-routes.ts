f2d0031c192ae363611d9f1725a00b8164d243dd
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-28 21:02:25 +0500

    add hash

--- src/routes/users/users-routes.ts
+++ src/routes/users/users-routes.ts
@@ -4,9 +4,10 @@ import {AllUsersController} from "../../controllers/users/GetAllUsers";
 import {userCreateValidator} from "../../validators/userValidator";
 import {createUserController} from "../../controllers/users/CreateUser";
 import {deleteUserController} from "../../controllers/users/DeleteUserToId";
+import {inputCheckErrorsMiddleware} from "../../utils/middlewares/checkErrorsValidator";
 
 export const userRouter: Router = Router();
 
 userRouter.get('/', adminMiddlewares, AllUsersController);
-userRouter.post('/', adminMiddlewares, ...userCreateValidator, createUserController);
+userRouter.post('/', adminMiddlewares, [...userCreateValidator, inputCheckErrorsMiddleware], createUserController);
 userRouter.delete('/:id', adminMiddlewares, deleteUserController);
