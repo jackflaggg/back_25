67b727ad51cf3ad0a6cb2f1f108c2f7fcea8d7e1
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-28 17:49:24 +0500

    add hash

--- src/utils/helpers/hash-helper.ts
+++ src/utils/helpers/hash-helper.ts
@@ -1,15 +1,19 @@
 import bcrypt from 'bcrypt'
 
 export const hashService = {
-    async _generateHash(password: string, salt: string) {
+    async _generateHash(password: string, saltRounds: number = 10) {
+        // генерируем соль
+        const salt = await bcrypt.genSalt(saltRounds);
+        // генерируем хэш на основе пароля и созданной соли
         return  await bcrypt.hash(password, salt)
     },
     async createPasswordHash(password: string) {
-        const passwordSalt = await bcrypt.hash(password, 10);
-        const passwordHash = await this._generateHash(password, passwordSalt);
-        return passwordHash;
+        // генерируем хэш пароля
+        // в коде используем данный метод для сокрытия логики первого!
+        return await this._generateHash(password);
     },
     async comparePassword(password: string, hash: string) {
+        // сравниваем пароль с хэшом
         return await bcrypt.compare(password, hash);
     }
 }
