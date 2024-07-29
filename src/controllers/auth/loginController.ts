ae72f9dbd64c53f4579779c390d52df293deaeb3
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-29 16:13:24 +0500

    add hash

--- src/controllers/auth/loginController.ts
+++ src/controllers/auth/loginController.ts
@@ -5,9 +5,9 @@ import {hashService} from "../../utils/helpers/helper-hash";
 
 export const loginController = async (req: Request, res: Response) => {
     console.log(req.body)
-    const {login, email, password} = req.body;
-
-    const searchElem = await authQueryRepository.findUserByLoginOrEmail(login || email)
+    const {loginOrEmail, password} = req.body;
+    console.log(loginOrEmail)
+    const searchElem = await authQueryRepository.findUserByLoginOrEmail(loginOrEmail)
 
     if (!searchElem) {
         res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION);
--- src/domain/auth/auth-service.ts
+++ src/domain/auth/auth-service.ts
@@ -1,13 +1,13 @@
 import {UsersDbRepository} from "../../repositories/users/users-db-repository";
 import {hashService} from "../../utils/helpers/helper-hash";
 
-export const authService = {
-    async loginUser(loginOrEmail: any, password: any): Promise<any | null> {
-        const userAuth = await this.authenticateUser(loginOrEmail, password)
-        return null
-    },
-    async authenticateUser(loginOrEmail: any, password: any): Promise<string | null> {
-        const userAuth = await UsersDbRepository.findByLoginOrEmail(loginOrEmail)
-        return null;
-    }
-}
+// export const authService = {
+//     async loginUser(loginOrEmail: any, password: any): Promise<any | null> {
+//         const userAuth = await this.authenticateUser(loginOrEmail, password)
+//         return null
+//     },
+//     async authenticateUser(loginOrEmail: any, password: any): Promise<string | null> {
+//         const userAuth = await UsersDbRepository.findByLoginOrEmail(loginOrEmail)
+//         return null;
+//     }
+// }
