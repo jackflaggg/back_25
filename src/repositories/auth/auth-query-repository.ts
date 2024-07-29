6e2613ae4865112671dba48baab6fecff1b11a15
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-29 11:22:45 +0500

    add hash

--- src/repositories/auth/auth-query-repository.ts
+++ src/repositories/auth/auth-query-repository.ts
@@ -4,11 +4,11 @@ export const authQueryRepository = {
     async findUserByLoginOrEmail(loginOrEmail: string): Promise<any> {
         const filter = {
             $or: [
-                {login: {$regex: loginOrEmail, $options: 'i'}} ,
-                {email: {$regex: loginOrEmail, $options: 'i'}}
+                {login: {$regex: loginOrEmail}} ,
+                {email: {$regex: loginOrEmail}}
                 ]
         }
-        console.log(await usersCollection.findOne(filter))
+
         return await usersCollection.findOne(filter);
     }
 }
