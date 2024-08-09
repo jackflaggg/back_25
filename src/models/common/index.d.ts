d7fba1a737f48b55182fd3fd9f52615a5ed5fe1e
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-09 19:02:17 +0500

    add hash

--- src/models/common/index.d.ts
+++ src/models/common/index.d.ts
@@ -1,10 +1,9 @@
-import {JwtPayload} from "jsonwebtoken";
 import {UserDbType} from "../db/db.models";
 
 declare global {
     namespace Express {
         export interface Request {
-            userId?: UserDbType | null | string | number //JwtPayload
+            userId?: UserDbType | null | string
         }
     }
 }
