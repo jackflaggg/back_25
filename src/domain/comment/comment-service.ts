2ed6b2842366a62f1a7666c0a71609e71ee2f479
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-10 23:30:30 +0500

    add hash

--- src/domain/comment/comment-service.ts
+++ src/domain/comment/comment-service.ts
@@ -1,11 +1,11 @@
 export const commentService = {
-    async createComment(existingCommentId: string, userId: string, commentId: string) {
+    async getComment(commentId: string) {
 
     },
     async updateComment(commentId: string){
 
     },
     async deleteComment(commentId: string){
-
+        
     },
 }
