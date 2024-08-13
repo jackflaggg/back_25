c65c1fe286597de4d370bfe8bd1b20ceb77e87ca
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-13 22:14:57 +0500

    add hash

--- src/controllers/auth/getInfoUserController.ts
+++ src/controllers/auth/getInfoUserController.ts
@@ -3,14 +3,14 @@ import { Request, Response} from 'express';
 import {AsyncVoid, HTTP_STATUSES} from "../../models/common/common-types";
 
 export const getInfoUserController = async (req: Request, res: Response): AsyncVoid => {
-    const existingId = req.userId;
+    const existingId = req.userId as string;
 
     if (!existingId) {
         res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION);
         return
     }
 
-    const user = await usersQueryRepository.LoginMapByUser(existingId as string)
+    const user = await usersQueryRepository.LoginMapByUser(existingId)
     if (!user) {
         res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION);
         return;
