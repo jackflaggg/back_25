b3c63df1600f46037eba7c9a8a11450fd394ed04
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-13 23:55:20 +0500

    add hash

--- src/controllers/posts/getAllPosts.ts
+++ src/controllers/posts/getAllPosts.ts
@@ -1,7 +1,7 @@
 import {Response} from "express";
 import {HTTP_STATUSES, RequestWithQuery} from "../../models/common/common-types";
 import {QueryPostInputModels} from "../../models/post/input/get-query.post.input.models";
-import { queryHelperToBlog, queryHelperToPost} from "../../utils/helpers/helper-query-get";
+import {queryHelperToPost} from "../../utils/helpers/helper-query-get";
 import {postsQueryRepository} from "../../repositories/posts/posts-query-repository";
 
 export const AllPostController = async (req: RequestWithQuery<QueryPostInputModels>,
