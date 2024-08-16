19957b45bd9c4ce95b108e67ef2472611a701295
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-16 23:54:30 +0500

    add helper

--- src/controllers/auth/loginController.ts
+++ src/controllers/auth/loginController.ts
@@ -1,8 +1,8 @@
 import {AccessToken, HTTP_STATUSES, RequestWithBody, ResponseBody} from "../../models/common/common-types";
-import {loginControllerModels} from "../../models/auth/input/login-post-controller";
+import {InLoginModels, loginControllerModels} from "../../models/auth/input/login-post-controller";
 import {authService} from "../../domain/auth/auth-service";
 
-export const loginController = async (req: RequestWithBody<loginControllerModels>, res: ResponseBody<AccessToken>) => {
+export const loginController = async (req: RequestWithBody<InLoginModels>, res: ResponseBody<AccessToken>) => {
 
     const loginUser = await authService.loginUser(req.body);
 
--- src/models/auth/input/login-post-controller.ts
+++ src/models/auth/input/login-post-controller.ts
@@ -1,4 +1,4 @@
-export type loginControllerModels = {
+export type InLoginModels = {
     loginOrEmail: string,
     password: string
 }
