e1b91d61031ed66d88922cacaacd04371de139db
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-19 00:37:32 +0500

    add helper

--- src/controllers/posts/CreateCommentsToPost.ts
+++ src/controllers/posts/CreateCommentsToPost.ts
@@ -1,6 +1,10 @@
 import {Request, Response} from "express"
 import {usersQueryRepository} from "../../repositories/users/users-query-repository";
 import {postsQueryRepository} from "../../repositories/posts/posts-query-repository";
+import {HTTP_STATUSES} from "../../models/common/common-types";
+import {CommentsDbRepository} from "../../repositories/comments/comments-db-repository";
+import {CommentsQueryRepository} from "../../repositories/comments/comments-query-repository";
+
 
 
 export const createCommentByPostIdController = async (req: Request, res: Response) => {
@@ -10,19 +14,19 @@ export const createCommentByPostIdController = async (req: Request, res: Respons
     const { postId } = req.params;
 
     if (!userId || !content || !postId) {
-        res.status(400).send({error: 'User || content not valid'});
+        res.status(HTTP_STATUSES.BAD_REQUEST_400).send({error: 'User || content not valid'});
         return
     }
     const findUser = await usersQueryRepository.getUserById(userId.toString());
     if (!findUser) {
-        res.status(400).send({error: 'not user'});
+        res.status(HTTP_STATUSES.BAD_REQUEST_400).send({error: 'not user'});
         return
     }
 
     const findPost = await postsQueryRepository.giveOneToIdPost(postId);
     console.log(findPost)
     if (!findPost) {
-        res.status(404).send({error: 'not found post'});
+        res.status(HTTP_STATUSES.NOT_FOUND_404).send({error: 'not found post'});
         return
     }
     const newPost = {
@@ -34,6 +38,21 @@ export const createCommentByPostIdController = async (req: Request, res: Respons
         },
         createdAt: new Date().toISOString(),
     }
-    res.status(200).send(newPost);
+
+    const newComment = await CommentsDbRepository.CreateComment(newPost);
+
+    if (!newComment) {
+        res.status(403).send({error: 'error on create comment!'});
+        return;
+    }
+
+    const findCreateComment = await CommentsQueryRepository.getComment(newComment);
+
+    if (!findCreateComment) {
+        res.status(HTTP_STATUSES.NOT_FOUND_404).send({error: 'not found comment'});
+        return
+    }
+
+    res.status(HTTP_STATUSES.OK_200).send(newPost);
     return;
 }
--- src/repositories/comments/comments-query-repository.ts
+++ src/repositories/comments/comments-query-repository.ts
@@ -1,21 +1,24 @@
-// import {commentsCollection, usersCollection} from "../../db/db";
-// import {ObjectId} from "mongodb";
-//
-// export const CommentsDbRepository = {
-//     async CreateComment(inputComment: any) {
-//         const comment = await commentsCollection.insertOne(inputComment);
-//         return comment.insertedId.toString();
-//     },
-//     async UpdateComment(findComment: any, updateDataComment: any): Promise<string | null> {
-//         const updateComment = await commentsCollection.updateOne(findComment, {$set: updateDataComment})
-//
-//         if (!updateComment || !updateComment.insertedId) {
-//             return null;
-//         }
-//         return updateComment.insertedId.toString();
-//     },
-//     async deleteComment(id: string): Promise<any> {
-//         const deleteComment = await usersCollection.deleteOne({_id: new ObjectId(id)});
-//         return deleteComment.acknowledged;
-//     },
-// }
+import {commentsCollection, usersCollection} from "../../db/db";
+import {ObjectId} from "mongodb";
+
+export const CommentsQueryRepository = {
+    async getComment(idComment: string) {
+        const comment = await commentsCollection.insertOne(inputComment);
+        if (!comment || !comment.insertedId) {
+            return null;
+        }
+        return comment.insertedId.toString();
+    },
+    // async UpdateComment(findComment: any, updateDataComment: any): Promise<string | null> {
+    //     const updateComment = await commentsCollection.updateOne(findComment, {$set: updateDataComment})
+    //
+    //     if (!updateComment || !updateComment.insertedId) {
+    //         return null;
+    //     }
+    //     return updateComment.insertedId.toString();
+    // },
+    // async deleteComment(id: string): Promise<any> {
+    //     const deleteComment = await usersCollection.deleteOne({_id: new ObjectId(id)});
+    //     return deleteComment.acknowledged;
+    // },
+}
