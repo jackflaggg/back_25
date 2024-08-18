54ab43f25492ad778cbee062de835096d74f4080
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-18 22:54:08 +0500

    add helper

--- src/utils/application/jwt-service.ts
+++ src/utils/application/jwt-service.ts
@@ -1,10 +1,19 @@
 import {SETTINGS} from "../../settings";
 import jwt, {JwtPayload} from "jsonwebtoken";
+import {config} from 'dotenv'
+
+config()
 
 export const jwtService = {
     async createToken(userId: string | null): Promise<null | string> {
         try {
             console.log(SETTINGS.SECRET_KEY)
+
+            if (!SETTINGS.SECRET_KEY) {
+                console.error('SECRET_KEY не установлен');
+                return null;
+                //throw new Error('SECRET_KEY не установлен');
+            }
             return jwt.sign(
                 {userId: userId},
                 SETTINGS.SECRET_KEY,
