ea754443794b940670f5fb6ae55d61382d85f481
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-22 21:13:25 +0500

    add registr

---
+++ src/controllers/auth/registrationController.ts
@@ -0,0 +1,5 @@
+import {Request, Response} from 'express';
+
+export const registrationController = async (req: Request, res: Response) => {
+
+}
--- src/routes/auth/auth-router.ts
+++ src/routes/auth/auth-router.ts
@@ -9,14 +9,17 @@ import {
 import {inputCheckErrorsMiddleware} from "../../utils/middlewares/checkErrorsValidator";
 import {authBearerMiddlewares} from "../../utils/middlewares/auth-bearer-middleware";
 import {getInfoUserController} from "../../controllers/auth/getInfoUserController";
+import {registrationConfirmationController} from "../../controllers/auth/registrConfirmationController";
+import {registrationController} from "../../controllers/auth/registrationController";
+import {registrationEmailController} from "../../controllers/auth/registrationEmailController";
 
 
 export const authRouter: Router = Router();
 
 
 authRouter.post('/login', [...loginPostValidator, inputCheckErrorsMiddleware], loginController);
-authRouter.post('/registration-confirmation', codeValidator, inputCheckErrorsMiddleware, );
-authRouter.post('/registration', [...registrationPostValidator, inputCheckErrorsMiddleware], );
-authRouter.post('/registration-email', emailValidator, inputCheckErrorsMiddleware, );
+authRouter.post('/registration-confirmation', codeValidator, inputCheckErrorsMiddleware, registrationConfirmationController);
+authRouter.post('/registration', [...registrationPostValidator, inputCheckErrorsMiddleware], registrationController);
+authRouter.post('/registration-email', emailValidator, inputCheckErrorsMiddleware, registrationEmailController);
 
 authRouter.get('/me', authBearerMiddlewares, getInfoUserController);
