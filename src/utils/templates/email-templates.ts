254e77aed2b4fe6ca2cfca8247dceee5fd18886d
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-23 13:40:08 +0500

    add registr

--- src/utils/templates/email-templates.ts
+++ src/utils/templates/email-templates.ts
@@ -1,7 +1,6 @@
 export const emailTemplates = {
     registrationEmailTemplate(code: string) {
-        return `
-        <h1>Thanks for your registration</h1>
+        return `<h1>Thanks for your registration</h1>
         <p>To finish registration please follow the link below:
         <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
         </p>`
