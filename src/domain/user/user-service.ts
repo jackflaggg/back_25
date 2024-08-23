bccf23503d47584a63cd14fc687c2824ff84bb99
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-23 16:13:36 +0500

    add registr

--- src/domain/user/user-service.ts
+++ src/domain/user/user-service.ts
@@ -2,12 +2,11 @@ import {UsersDbRepository} from "../../repositories/users/users-db-repository";
 import {HTTP_STATUSES, ResultError} from "../../models/common/common-types";
 import {hashService} from "../../utils/application/hash-service";
 import {errorsValidate} from "../../utils/features/errors-validate";
-import {userMapToCreateAdmin} from "../../utils/mappers/user-mapper";
 import {OutUserServiceModel} from "../../models/user/ouput/output-type-users";
 import {emailConfirmation} from "../../utils/features/emailConfirmation";
 
 export const userService = {
-    async createUser(user: Omit<OutUserServiceModel, 'createdAt'>): Promise<string | ResultError | null> {
+    async createUser(user: Omit<OutUserServiceModel, 'createdAt' | 'emailConfirmation'>): Promise<string | ResultError | null> {
         const { login, password, email} = user;
 
         const errors = await errorsValidate( email, login );
@@ -26,7 +25,7 @@ export const userService = {
             email,
             password,
             createdAt: new Date().toISOString(),
-            emailConfirmation: emailConfirmation
+            emailConfirmation: emailConfirmation()
         }
 
         return await UsersDbRepository.createUser(newUser);
