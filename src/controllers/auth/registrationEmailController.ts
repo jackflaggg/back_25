d6fb63627b0caec51e963db1184306e10f3c4ed3
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-24 22:08:55 +0500

    add unique error

--- src/controllers/auth/registrationEmailController.ts
+++ src/controllers/auth/registrationEmailController.ts
@@ -7,7 +7,7 @@ import {authService} from "../../domain/auth/auth-service";
 export const registrationEmailController = async (req: Request, res: Response) => {
     const findEmail = await authService.registrationEmailResending(req.params.email);
     if (findEmail.status !== ResultSuccess.Success) {
-        res.status(HTTP_STATUSES.BAD_REQUEST_400).send(findEmail.extensions);
+        res.status(HTTP_STATUSES.BAD_REQUEST_400).send(JSON.parse(JSON.stringify(findEmail.extensions)));
         return;
     }
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
