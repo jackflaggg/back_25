81f5045e0921804a677253834a859bde18f641ca
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-25 22:29:21 +0500

    add unique error

--- src/controllers/auth/loginController.ts
+++ src/controllers/auth/loginController.ts
@@ -17,6 +17,9 @@ export const loginController = async (req: RequestWithBody<InLoginModels>, res: 
         return;
     }
 
-    res.status(HTTP_STATUSES.OK_200).send({accessToken: loginUser.data[0]});
+    const [accessToken, refreshToken] = loginUser.data;
+
+    res.cookie('refreshToken', refreshToken, {httpOnly: true, secure: true});
+    res.status(HTTP_STATUSES.OK_200).send({accessToken: accessToken});
     return;
 }
