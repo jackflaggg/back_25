5b73cedabd034ffee834ffe403c1ccbcbd5897a5
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-26 00:28:26 +0500

    add unique error

--- src/utils/application/jwt-service.ts
+++ src/utils/application/jwt-service.ts
@@ -37,7 +37,8 @@ export const jwtService = {
     // этот метод проверяет действительность токена + проверка на срок действия
     // Если токен действителен, метод возвращает объект с данными JwtPayload (payload)
     async verifyAccessToken(token: string): Promise<null | JwtPayload >  {
-        console.log('начало:' + SETTINGS.EXPIRES_IN_ACCESS_TOKEN)
+        console.log('начало 500 :' + SETTINGS.EXPIRES_IN_ACCESS_TOKEN)
+        console.log('че пришло в 500: ' + token);
         try {
             return jwt.verify(token, SETTINGS.SECRET_KEY) as JwtPayload | null
         } catch (error: unknown) {
