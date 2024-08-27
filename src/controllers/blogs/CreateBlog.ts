f9f9a6e37551d3b3f2a7ff0e20353b1e03077dbc
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-27 22:45:32 +0500

    add unique error

--- src/controllers/blogs/CreateBlog.ts
+++ src/controllers/blogs/CreateBlog.ts
@@ -1,11 +1,11 @@
 import {HTTP_STATUSES, RequestWithBody, ResponseBody} from "../../models/common/common-types";
-import {OutputBlogModel} from "../../models/blog/output/output-type-blogs";
+import {OutBlogModel} from "../../models/blog/output/output-type-blogs";
 import {blogsService} from "../../domain/blog/blog-service";
 import {blogsQueryRepositories} from "../../repositories/blogs/blogs-query-repository";
 import {InCreateBlogModel} from "../../models/blog/input/input-type-blogs";
 
 export const createBlogController = async (req: RequestWithBody<InCreateBlogModel>,
-                                           res:ResponseBody<OutputBlogModel>) => {
+                                           res:ResponseBody<OutBlogModel>) => {
     const createdBlogId = await blogsService.createBlog(req.body);
 
     const blog = await blogsQueryRepositories.giveOneToIdBlog(createdBlogId!);
