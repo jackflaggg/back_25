176e561523050b6eb3958f84e00a5ab80bfd8d0e
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-27 22:55:02 +0500

    add unique error

--- src/controllers/posts/getAllPosts.ts
+++ src/controllers/posts/getAllPosts.ts
@@ -1,8 +1,9 @@
-import {HTTP_STATUSES, RequestWithQuery, ResponseBody} from "../../models/common/common-types";
+import {HTTP_STATUSES} from "../../models/common/common-types";
 import {queryHelperToPost} from "../../utils/helpers/helper-query-get";
 import {postsQueryRepository} from "../../repositories/posts/posts-query-repository";
 import {InQueryPostModel} from "../../models/post/input/input-type-posts";
 import {OutGetAllPosts} from "../../models/post/output/output-type-posts";
+import {RequestWithQuery, ResponseBody} from "../../models/req_res_params/request-response-params";
 
 export const AllPostController = async (req: RequestWithQuery<InQueryPostModel>,
                                         res:ResponseBody<OutGetAllPosts>) => {
