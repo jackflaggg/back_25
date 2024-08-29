4358a94f76b41f4520681c61d5bef4e065e3cd61
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-30 00:50:13 +0500

    del controller

--- src/controllers/comments/DeleteCommentId.ts
+++ src/controllers/comments/DeleteCommentId.ts
@@ -1,11 +1,10 @@
 import {Response} from "express";
 import {commentService} from "../../domain/comment/comment-service";
 import {HTTP_STATUSES} from "../../models/common/common-types";
-import {ErrorsType, ResultStatus, ResultStatusType} from "../../models/common/errors/errors-type";
+import {ResultStatus, ResultStatusType} from "../../models/common/errors/errors-type";
 import {
     CommentParamsId,
     RequestWithParams,
-    ResponseBody
 } from "../../models/common/req_res_params/request-response-params";
 import {errorsMessages} from "../../utils/features/errorsMessages";
 
