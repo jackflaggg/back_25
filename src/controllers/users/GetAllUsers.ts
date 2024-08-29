0e3bdb4440db210da1aef8aaaab2b475b7bdce3f
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-29 23:54:22 +0500

    del controller

--- src/controllers/users/GetAllUsers.ts
+++ src/controllers/users/GetAllUsers.ts
@@ -2,11 +2,12 @@ import {HTTP_STATUSES} from "../../models/common/common-types";
 import {Response} from "express";
 import {usersQueryRepository} from "../../repositories/users/users-query-repository";
 import {queryHelperToUser} from "../../utils/helpers/helper-query-get";
-import {RequestWithQuery} from "../../models/common/req_res_params/request-response-params";
+import {RequestWithQuery, ResponseBody} from "../../models/common/req_res_params/request-response-params";
 import {InQueryUserModel} from "../../models/user/helper-query-user/helper-user";
+import {OutQueryCreateUsersModel} from "../../models/user/ouput/output-type-users";
 
 export const AllUsersController = async (req: RequestWithQuery<InQueryUserModel>,
-                                        res:Response) => {
+                                        res:ResponseBody<OutQueryCreateUsersModel>) => {
     const queryUser = queryHelperToUser(req.query);
 
     const getAllUsers = await usersQueryRepository.getAllUsers(queryUser)
