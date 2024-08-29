9f1afdd8dbd0e8e79fff8cfd67a7c707a648dec8
jackflaggg <rasul.khamzinnn@gmail.com>
2024-08-30 00:08:33 +0500

    del controller

--- src/utils/mappers/user-mapper.ts
+++ src/utils/mappers/user-mapper.ts
@@ -1,27 +1,6 @@
 import {emailConfirmation} from "../features/emailConfirmation";
 import {OutUserById} from "../../models/user/ouput/output-type-users";
 
-
-// export const userMapper = (user: any)=> {
-//     return {
-//         id: user._id.toString(),
-//         login: user.login,
-//         email: user.email,
-//         createdAt: user.createdAt,
-//         emailConfirmation
-//     }
-// }
-//
-// export const userMapperToCreate = (login: string, email: string, password: string) => {
-//     return {
-//         login,
-//         email,
-//         password,
-//         createdAt: new Date().toISOString(),
-//         emailConfirmation
-//     }
-// }
-
 export const userMapToCreateAdmin = (login: string, email: string, password: string) => {
     return {
         login,
