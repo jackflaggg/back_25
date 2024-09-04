34444043d92f4ce885dd784cc33dfb179e26125f
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-04 23:50:17 +0500

    del controller

--- __tests__/unit-test/auth/auth.service.spec.ts
+++ __tests__/unit-test/auth/auth.service.spec.ts
@@ -5,6 +5,7 @@ import { hashService } from '../../../src/utils/application/hash-service';
 import {OutUserFindLoginOrEmail} from "../../../src/models/user/ouput/output-type-users";
 import {ObjectId} from "mongodb";
 import {createString} from "../../helpers-e2e/datatests";
+import {jwtService} from "../../../src/utils/application/jwt-service";
 
 let outUser : OutUserFindLoginOrEmail ={
     _id: new ObjectId(),
@@ -39,6 +40,7 @@ jest.mock('../../../src/managers/email-managers', () => ({
         sendEmailRecoveryMessage: jest.fn()
     },
 }));
+
 
 describe('authService', () => {
     beforeEach(() => {
@@ -85,5 +87,80 @@ describe('authService', () => {
         });
     });
 
-    // Остальные describe блоки
+    describe('loginUser', () => {
+        it('⛔ возвращает ошибку, если аутентификация рухнула', async () => {
+            (UsersDbRepository.findUserByLoginOrEmail as jest.Mock).mockResolvedValueOnce(null);
+
+            (hashService.comparePassword as jest.Mock).mockResolvedValueOnce(false);
+
+            const loginErrorResponse = {
+                status: ResultStatus.BadRequest,
+                extensions: { field: 'userId', message: 'Аутентификация рухнула!' },
+                data: null
+            };
+
+            const response = await authService.loginUser({
+                loginOrEmail: createString(10), password: createString(11)
+            });
+
+            expect(response).toEqual(loginErrorResponse);
+        });
+
+        it('⛔ возвращает ошибку, если в access токене null', async () => {
+
+            (UsersDbRepository.findUserByLoginOrEmail as jest.Mock).mockResolvedValueOnce(outUser);
+
+            (hashService.comparePassword as jest.Mock).mockResolvedValueOnce(true);
+
+            (jwtService.createAnyToken as jest.Mock).mockResolvedValueOnce(null)
+
+            const response = await authService.loginUser({
+                loginOrEmail: createString(10), password: createString(11)
+            });
+
+            expect(response).toEqual({
+                status: ResultStatus.BadRequest,
+                extensions: {field: 'jwt', message: 'Проблема при генерации Access токена!'},
+                data: null
+            });
+        });
+
+        it('⛔ возвращает ошибку, если в refresh токене null', async () => {
+
+            (UsersDbRepository.findUserByLoginOrEmail as jest.Mock).mockResolvedValueOnce(outUser);
+
+            (hashService.comparePassword as jest.Mock).mockResolvedValueOnce(true);
+
+            (jwtService.createAnyToken as jest.Mock)
+                .mockResolvedValueOnce('accessToken')
+                .mockResolvedValueOnce(null)
+
+            const response = await authService.loginUser({
+                loginOrEmail: createString(10), password: createString(11)
+            });
+
+            expect(response).toEqual({
+                status: ResultStatus.BadRequest,
+                extensions: {field: 'jwt', message: 'Проблема при генерации Refresh токена!'},
+                data: null
+            });
+        });
+
+        it('✅ проходит успешно', async () => {
+
+            (UsersDbRepository.findUserByLoginOrEmail as jest.Mock).mockResolvedValue(outUser);
+
+            (hashService.comparePassword as jest.Mock).mockResolvedValue(true);
+
+            (jwtService.createAnyToken as jest.Mock)
+                .mockResolvedValue('access')
+                .mockResolvedValue('refresh')
+
+            const response = await authService.authenticationUserToLogin({ loginOrEmail: 'success', password: 'success_password' });
+            expect(response).toEqual({
+                status: ResultSuccess.Success,
+                data: [createString(17), createString(16)]
+            });
+        });
+    });
 });
