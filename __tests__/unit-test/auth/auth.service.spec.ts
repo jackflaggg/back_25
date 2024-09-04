f8838ecf2b90f3f222a336a2d479b14d02c71029
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-04 22:19:39 +0500

    del controller

--- __tests__/unit-test/auth/auth.service.spec.ts
+++ __tests__/unit-test/auth/auth.service.spec.ts
@@ -3,6 +3,7 @@ import {authService} from "../../../src/domain/auth/auth-service";
 import {ResultStatus, ResultSuccess} from "../../../src/models/common/errors/errors-type";
 import {hashService} from "../../../src/utils/application/hash-service";
 import {jwtService} from "../../../src/utils/application/jwt-service";
+import {emailManagers} from "../../../src/managers/email-managers";
 
 jest.mock('../../../src/repositories/users/users-db-repository');
 jest.mock('../../../src/utils/application/hash-service');
@@ -18,7 +19,7 @@ describe('authService', () => {
         it('should return error if user not found', async () => {
             (UsersDbRepository.findUserByLoginOrEmail as jest.Mock).mockResolvedValue(null);
 
-            const response = await authService.authenticationUserToLogin({ loginOrEmail: 'test', password: 'password' });
+            const response = await authService.authenticationUserToLogin({ loginOrEmail: 'invalid', password: 'password' });
             expect(response).toEqual({
                 status: ResultStatus.BadRequest,
                 extensions: { field: 'user', message: 'Пользователь не найден!' },
@@ -27,11 +28,11 @@ describe('authService', () => {
         });
 
         it('should return error if password is incorrect', async () => {
-            const mockUser = { password: 'hashedpassword' };
+            const mockUser = { password: 'hashedPassword' };
             (UsersDbRepository.findUserByLoginOrEmail as jest.Mock).mockResolvedValue(mockUser);
             (hashService.comparePassword as jest.Mock).mockResolvedValue(false);
 
-            const response = await authService.authenticationUserToLogin({ loginOrEmail: 'test', password: 'wrongpassword' });
+            const response = await authService.authenticationUserToLogin({ loginOrEmail: 'email', password: 'wrongPassword' });
             expect(response).toEqual({
                 status: ResultStatus.BadRequest,
                 extensions: { field: 'hashService', message: 'Пароль не прошел проверку!' },
@@ -39,12 +40,12 @@ describe('authService', () => {
             });
         });
 
-        it('should return user id on successful login', async () => {
-            const mockUser = { _id: 'userId', password: 'hashedpassword' };
+        it('should return user ID on successful login', async () => {
+            const mockUser = { _id: 'userId', password: 'hashedPassword' };
             (UsersDbRepository.findUserByLoginOrEmail as jest.Mock).mockResolvedValue(mockUser);
             (hashService.comparePassword as jest.Mock).mockResolvedValue(true);
 
-            const response = await authService.authenticationUserToLogin({ loginOrEmail: 'test', password: 'password' });
+            const response = await authService.authenticationUserToLogin({ loginOrEmail: 'email', password: 'password' });
             expect(response).toEqual({
                 status: ResultSuccess.Success,
                 data: 'userId'
@@ -54,13 +55,13 @@ describe('authService', () => {
 
     describe('loginUser', () => {
         it('should return error if authentication fails', async () => {
-            (authService.authenticationUserToLogin as jest.Mock).mockResolvedValueOnce({
+            (authService.authenticationUserToLogin as jest.Mock).mockResolvedValue({
                 status: ResultStatus.BadRequest,
                 extensions: { field: 'userId', message: 'Аутентификация рухнула!' },
                 data: null
             });
 
-            const response = await authService.loginUser({ loginOrEmail: 'test', password: 'password' });
+            const response = await authService.loginUser({ loginOrEmail: 'email', password: 'password' });
             expect(response).toEqual({
                 status: ResultStatus.BadRequest,
                 extensions: { field: 'userId', message: 'Аутентификация рухнула!' },
@@ -69,14 +70,13 @@ describe('authService', () => {
         });
 
         it('should return tokens on successful login', async () => {
-            (authService.authenticationUserToLogin as jest.Mock).mockResolvedValueOnce({
+            (authService.authenticationUserToLogin as jest.Mock).mockResolvedValue({
                 status: ResultSuccess.Success,
                 data: 'userId'
             });
-            (jwtService.createAnyToken as jest.Mock).mockResolvedValueOnce('token');
-            (jwtService.createAnyToken as jest.Mock).mockResolvedValueOnce('token');
+            (jwtService.createAnyToken as jest.Mock).mockResolvedValue('token');
 
-            const response = await authService.loginUser({ loginOrEmail: 'test', password: 'password' });
+            const response = await authService.loginUser({ loginOrEmail: 'email', password: 'password' });
             expect(response).toEqual({
                 status: ResultSuccess.Success,
                 data: ['token', 'token']
@@ -88,20 +88,29 @@ describe('authService', () => {
         it('should return error if required fields are missing', async () => {
             const response = await authService.registrationUser({ login: '', password: '', email: '' });
             expect(response.status).toEqual(ResultStatus.BadRequest);
-            //expect(response.extensions).toMatch('{message: `${key} is required`, field: `${}`');
+            expect(response?.extensions?.message).toMatch(/is required/);
         });
 
-        // Добавьте дополнительные тесты для проверки уникальности, успешного создания пользователя и т.д.
-    });
+        it('should return error if user already exists', async () => {
+            (UsersDbRepository.createUser as jest.Mock).mockResolvedValue(null);
+            (hashService._generateHash as jest.Mock).mockResolvedValue('hashedPassword');
+            (emailManagers.sendEmailRecoveryMessage as jest.Mock).mockResolvedValue(true);
+            (UsersDbRepository.findUserByLoginOrEmail as jest.Mock).mockResolvedValueOnce({ email: 'existing@example.com' });
 
-    describe('confirmationEmailByCode', () => {
-        it('should return error if required bad code', async () => {
-            const response = await authService.confirmationEmailByCode('bad code');
-            expect(response.status).toEqual(ResultStatus.BadRequest);
-            //expect(response.extensions).toMatch('{message: \'code 1\', field: \'code\'}');
-            expect(response.data).toBeNull()
+            const response = await authService.registrationUser({ login: 'existing', password: 'password', email: 'existing@example.com' });
+            expect(response.status).toEqual(ResultSuccess.Success);
         });
 
-        // Добавьте дополнительные тесты для проверки уникальности, успешного создания пользователя и т.д.
+        it('should successfully register a user', async () => {
+            (UsersDbRepository.createUser as jest.Mock).mockResolvedValue('newUserId');
+            (hashService._generateHash as jest.Mock).mockResolvedValue('hashedPassword');
+            (emailManagers.sendEmailRecoveryMessage as jest.Mock).mockResolvedValue(true);
+
+            const response = await authService.registrationUser({ login: 'newUser', password: 'password', email: 'newUser@example.com' });
+            expect(response).toEqual({
+                status: ResultSuccess.Success,
+                data: 'newUserId'
+            });
+        });
     });
 });
