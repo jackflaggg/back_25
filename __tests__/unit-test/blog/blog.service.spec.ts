6e54ef67103e33280dc313cfa387b23e5dc60062
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-05 23:02:07 +0500

    del controller

--- __tests__/unit-test/blog/blog.service.spec.ts
+++ __tests__/unit-test/blog/blog.service.spec.ts
@@ -1,4 +1,35 @@
+import {blogsRepositories} from "../../../src/repositories/blogs/blogs-db-repository";
+import {createString} from "../../helpers-e2e/datatests";
+import {ObjectId} from "mongodb";
+import {blogsService} from "../../../src/domain/blog/blog-service";
+import {InCreatePostToBlogInputModel} from "../../../src/models/post/input/input-type-posts";
 
+jest.mock('../../../src/repositories/blogs/blogs-db-repository', () => ({
+    blogsRepositories: {
+        createBlog: jest.fn(),
+        putBlog: jest.fn(),
+        createPostToBlogID: jest.fn(),
+        delBlog: jest.fn(),
+    }
+}));
+
+let createBlogData = {
+    id: new ObjectId().toString(),
+    name: createString(10),
+    description: createString(10),
+    websiteUrl: createString(10),
+    createdAt: new Date().toISOString(),
+    isMembership: false
+}
+
+let createPostToBlogModel = {
+    title: createString(10),
+    shortDescription: createString(10),
+    content: createString(10),
+    blogId: createBlogData.id,
+    blogName: createBlogData.name,
+    createdAt: new Date().toISOString(),
+}
 
 describe('blogsService', () => {
     beforeEach(() => {
@@ -6,8 +37,63 @@ describe('blogsService', () => {
     });
 
     describe('createBlog', () => {
-        it('creates blog success', () => {
+        it('creates blog success', async() => {
+            (blogsRepositories.createBlog as jest.Mock).mockResolvedValueOnce(createBlogData.id);
 
-        })
-    })
+            const response = await blogsService.createBlog({
+                name: createString(10),
+                description: createString(10),
+                websiteUrl: createString(10)});
+
+            expect(response).toBe(createBlogData.id)
+        });
+    });
+
+    describe('createPostToBlogInputModel', () => {
+        it('creates post to blog success', async() => {
+            const expectedId = new ObjectId().toString();
+            (blogsRepositories.createPostToBlogID as jest.Mock).mockResolvedValueOnce(expectedId);
+
+            let InCreateToBlogModel = {
+                id: new ObjectId().toString(),
+                name: createString(10),
+            }
+
+            let InCreatePostToBlogInputModel = {
+                title: createString(10),
+                shortDescription: createString(10),
+                content: createString(10)
+            }
+
+            const response = await blogsService.createPostToBlogInputModel(InCreateToBlogModel,InCreatePostToBlogInputModel);
+
+            expect(response).toBe(expectedId)
+        });
+    });
+
+    // describe('createBlog', () => {
+    //     it('creates blog success', async() => {
+    //         (blogsRepositories.createBlog as jest.Mock).mockResolvedValueOnce(createBlogData.id);
+    //
+    //         const response = await blogsService.createBlog({
+    //             name: createString(10),
+    //             description: createString(10),
+    //             websiteUrl: createString(10)});
+    //
+    //         expect(response).toBe(createBlogData.id)
+    //     });
+    // });
+    //
+    // describe('createBlog', () => {
+    //     it('creates blog success', async() => {
+    //         (blogsRepositories.createBlog as jest.Mock).mockResolvedValueOnce(createBlogData.id);
+    //
+    //         const response = await blogsService.createBlog({
+    //             name: createString(10),
+    //             description: createString(10),
+    //             websiteUrl: createString(10)});
+    //
+    //         expect(response).toBe(createBlogData.id)
+    //     });
+    // });
 })
