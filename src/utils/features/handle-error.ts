d48ac5d36b7eb63bf9ebf22e8ffcf96aa209fa2c
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-08 18:29:59 +0500

    del controller

--- src/utils/features/handle-error.ts
+++ src/utils/features/handle-error.ts
@@ -2,5 +2,5 @@ import {HTTP_STATUSES} from "../../models/common/common-types";
 import { Response} from "express";
 
 export const handleError = (res: Response, msg: string) => {
-    return  res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION_401);
+    return res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION_401);
 };
