25f85a4abebb1592531216bb550306b5394a1276
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-10 23:20:28 +0500

    del blogName to post-service

--- src/controllers/comments/DeleteCommentId.ts
+++ src/controllers/comments/DeleteCommentId.ts
@@ -13,7 +13,7 @@ export const deleteCommentController = async (req: RequestWithParams<CommentPara
 
     const { userId } = req;
 
-    const deleteComment = await commentService.deleteComment(commentId, userId as string)
+    const deleteComment = await commentService.deleteComment(commentId, userId as string);
 
     const statusMap: Record<ResultStatusType, HTTP_STATUSES> = {
         [ResultStatus.BadRequest]: HTTP_STATUSES.BAD_REQUEST_400,
