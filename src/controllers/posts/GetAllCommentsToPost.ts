49935298a62edf6a399620a16808c3f78a026904
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-10 23:04:33 +0500

    del blogName to post-service

--- src/controllers/posts/GetAllCommentsToPost.ts
+++ src/controllers/posts/GetAllCommentsToPost.ts
@@ -1,4 +1,3 @@
-import {Response, Request} from "express"
 import {HTTP_STATUSES} from "../../models/common/common-types";
 import {validateId} from "../../utils/helpers/helper-validate-id";
 import {postsQueryRepository} from "../../repositories/posts/posts-query-repository";
@@ -16,27 +15,27 @@ export const getCommentsToPostId = async (req: RequestWithParamsAndQuery<BlogPar
     const { postId } = req.params
 
     if(!validateId(postId)){
-        console.log('рухнула валидация!')
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-        return
+        console.log(`[postId] рухнула валидация`);
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
     }
 
     const existingPost = await postsQueryRepository.giveOneToIdPost(postId);
     if(!existingPost){
-        console.log('нет поста!')
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-        return
+        console.log(`[existingPost] не найден пост в репозитории`);
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
     }
 
     const sortDataQuery = queryHelperToPost(req.query);
 
     const allComments = await CommentsQueryRepository.getAllCommentsToPostId(postId, sortDataQuery);
     if (!allComments){
-        console.log('нет комментов!')
-        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-        return
+        console.log(`[allComments] выдал херню`);
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
     }
     res.status(HTTP_STATUSES.OK_200).send(allComments);
-    return
+    return;
 
 }
