0179d4da5f8a1083e538d6adc06dad8a6e9420be
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-10 22:41:33 +0500

    del blogName to post-service

--- src/validators/postValidator.ts
+++ src/validators/postValidator.ts
@@ -39,7 +39,7 @@ export const blogIdValidator =
     .custom(async blogId => {
         const blog = await blogsQueryRepositories.giveOneToIdBlog(blogId);
         if (!blog) {
-            throw Error('Incorrect blogId');
+            throw Error('некорректный blogId');
         }
         return !!blog;
     })
