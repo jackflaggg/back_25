041deee30e8b3127b08e3fd55b7bc90519dc36eb
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-10 23:13:53 +0500

    del blogName to post-service

--- src/controllers/posts/DelPost.ts
+++ src/controllers/posts/DelPost.ts
@@ -7,19 +7,21 @@ import {PostParamsId, RequestWithParams} from "../../models/common/req_res_param
 
 export const deletePostController = async (req: RequestWithParams<PostParamsId>,
                                            res:Response) => {
-    const { id: PostId } = req.params;
-    if (!validateId(PostId)) {
+    const { id: postId } = req.params;
+    if (!validateId(postId)) {
+        console.log(`[postId] не прошел валидацию`);
         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
         return
     }
 
-    const post = await postsQueryRepository.giveOneToIdPost(PostId);
+    const post = await postsQueryRepository.giveOneToIdPost(postId);
 
     if (!post) {
+        console.log(`[post] не был найден в репозитории`);
         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
         return;
     }
-    const delPost = await postsService.delPost(PostId);
+    const delPost = await postsService.delPost(postId);
 
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
     return;
