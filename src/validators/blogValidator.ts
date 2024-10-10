26ede48840bff12d5e4d09403aed6858d6e8c0e1
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-10 22:43:13 +0500

    del blogName to post-service

--- src/validators/blogValidator.ts
+++ src/validators/blogValidator.ts
@@ -3,32 +3,32 @@ import {body, ValidationChain} from "express-validator";
 export const nameValidator =
     body('name')
     .isString()
-    .withMessage('this is not string')
+    .withMessage('это не строка')
     .trim()
     .notEmpty()
     .withMessage('empty')
     .isLength({max: 15})
-    .withMessage('more then 15');
+    .withMessage('больше 15 символов');
 
 export const descriptionValidator =
     body('description')
     .isString()
-    .withMessage('this is not string')
+    .withMessage('это не строка')
     .trim()
     .notEmpty()
     .withMessage('empty')
     .isLength({max: 500})
-    .withMessage('more then 500');
+    .withMessage('больше 500 символов');
 
 export const urlValidator =
     body('websiteUrl')
     .isString()
-    .withMessage('this is not string')
+    .withMessage('это не строка')
     .trim()
     .notEmpty()
     .withMessage('empty')
     .isLength({max: 100})
-    .withMessage('more then 100')
+    .withMessage('больше 100 символов')
     .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
     .withMessage('websiteUrl must be a valid URL');
 
