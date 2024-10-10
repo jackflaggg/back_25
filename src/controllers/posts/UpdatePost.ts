c7f67e20bab0e5ec0fc9170d823f1efa5ec38dca
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-10 23:12:49 +0500

    del blogName to post-service

--- src/controllers/posts/UpdatePost.ts
+++ src/controllers/posts/UpdatePost.ts
@@ -12,24 +12,28 @@ export const updatePostController = async (req: RequestWithParamsAndBody<PostPar
     const { id : postId } = req.params;
 
     if (!validateId(postId)) {
+        console.log(`[postId] не валиден`);
         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
         return;
     }
 
     const post = await postsQueryRepository.giveOneToIdPost(postId);
     if (!post){
+        console.log(`[post] не был найден в репозитории`);
         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
         return;
     }
 
     const blog = await blogsQueryRepositories.giveOneToIdBlog(req.body.blogId);
     if (!blog){
+        console.log(`[blog] не был найден в репозитории`);
         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
         return;
     }
 
     const isUpdatedPost = await postsService.putPost(req.body, blog.name, postId);
     if (!isUpdatedPost) {
+        console.log(`[isUpdatedPost] вернул ошибку в сервисе`);
         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
         return;
     }
