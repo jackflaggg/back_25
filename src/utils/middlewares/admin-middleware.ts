68ea673c2c7c59edd0ab26d67ee09cf886777b1a
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-10 22:58:37 +0500

    del blogName to post-service

--- src/utils/middlewares/admin-middleware.ts
+++ src/utils/middlewares/admin-middleware.ts
@@ -7,12 +7,14 @@ export const adminMiddlewares = (req: Request, res: Response, next:NextFunction)
     const {authorization: auth} = req.headers;
 
     if (!auth || !auth?.startsWith('Basic')){
+        console.log(`[auth] ошибка в авторизации в заголовках`);
         res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION_401);
         return;
     }
 
     const codedAuthorization = fromUTF8ToBase64(SETTINGS.ADMIN);
     if(auth.slice(6) !== (codedAuthorization)){
+        console.log(`[codedAuthorization] ошибка в декодировании`);
         res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION_401);
         return;
     }
