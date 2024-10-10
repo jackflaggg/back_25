a5ad2b4d35c26c3cd6d737dde85431c265374e7a
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-10 23:33:32 +0500

    del blogName to post-service

--- src/controllers/auth/registrationController.ts
+++ src/controllers/auth/registrationController.ts
@@ -11,12 +11,12 @@ export const registrationController = async (req: RequestWithBody<InRegistration
 
     if (checkRegistrationUser.status !== ResultSuccess.Success) {
         if (checkRegistrationUser.errors) {
-            console.log(`[]`);
+            console.log(`[checkRegistrationUser.errors] возникли ошибки`);
             res.status(HTTP_STATUSES.BAD_REQUEST_400).send(errorsMessages(checkRegistrationUser.errors));
             return;
         }
         if (checkRegistrationUser.extensions) {
-            console.log(`[]`);
+            console.log(`[checkRegistrationUser.extensions] истек`);
             res.status(HTTP_STATUSES.BAD_REQUEST_400).send(errorsMessages(checkRegistrationUser.extensions));
             return;
         }
