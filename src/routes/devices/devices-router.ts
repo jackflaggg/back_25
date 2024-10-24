2360b9d1770dd7548f98cee16d2f8fbb800e4ba9
jackflaggg <rasul.khamzinnn@gmail.com>
2024-10-24 20:17:33 +0500

    del blogName to post-service

---
+++ src/routes/devices/devices-router.ts
@@ -0,0 +1,6 @@
+import {Router} from "express";
+
+export const devicesRouter = Router();
+devicesRouter.get("/devices",)
+devicesRouter.delete("/devices",)
+devicesRouter.delete("/devices/:deviceId",)
