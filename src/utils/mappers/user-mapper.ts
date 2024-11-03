3002d58469b1d9f7244e2992ed9ddbeb72e78f57
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-03 23:17:55 +0500

    Arrow Function with Implicit Return

--- src/utils/mappers/user-mapper.ts
+++ src/utils/mappers/user-mapper.ts
@@ -2,19 +2,15 @@ import {OutUserById} from "../../models/user/ouput/output-type-users";
 import {WithId} from "mongodb";
 import {UserDbType} from "../../models/db/db.models";
 
-export const userMapperToOutput = (user: WithId<UserDbType>)=> {
-    return {
+export const userMapperToOutput = (user: WithId<UserDbType>)=> ({
         id: user._id.toString(),
         login: user.login,
         email: user.email,
         createdAt: user.createdAt,
-    }
-}
+})
 
-export const loginUserMapper = (user: OutUserById) => {
-    return {
+export const loginUserMapper = (user: OutUserById) => ({
         email: user.email,
         login: user.login,
         userId: user.id
-    }
-}
+})
