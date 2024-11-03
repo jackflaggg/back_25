511d80a959d97e22aa01b86c7cbc672a72e0d4d3
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-03 23:17:22 +0500

    Arrow Function with Implicit Return

--- src/utils/mappers/post-mapper.ts
+++ src/utils/mappers/post-mapper.ts
@@ -2,14 +2,12 @@ import {WithId} from "mongodb";
 import {PostDbType} from "../../models/db/db.models";
 import {OutPostModel} from "../../models/post/output/output-type-posts";
 
-export const postMapper = (post: WithId<PostDbType>): OutPostModel => {
-    return {
+export const postMapper = (post: WithId<PostDbType>): OutPostModel => ({
         id: post._id.toString(),
         title: post.title,
         shortDescription: post.shortDescription,
         content: post.content,
         blogName: post.blogName,
         createdAt: post.createdAt,
-        blogId: post.blogId,
-    }
-}
+        blogId: post.blogId
+})
