5a58d94e41311c01542968d53dbd7421a1a90a22
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-07 17:52:00 +0500

    Arrow Function with Implicit Return

--- src/models/db/db.models.ts
+++ src/models/db/db.models.ts
@@ -1,55 +1,56 @@
 import {emailInfo} from "../user/ouput/output-type-users";
 
 export interface BlogDbType {
-    id?: string,
-    name: string,
-    description: string,
-    websiteUrl: string
-    createdAt: string,
-    isMembership: boolean
+    id?:            string,
+    name:           string,
+    description:    string,
+    websiteUrl:     string
+    createdAt:      string,
+    isMembership:   boolean
 }
 
 export interface PostDbType {
-    title: string,
-    shortDescription: string,
-    content: string,
-    blogId: string,
-    blogName: string,
-    createdAt: string
+    title:              string,
+    shortDescription:   string,
+    content:            string,
+    blogId:             string,
+    blogName:           string,
+    createdAt:          string
 }
 
 export interface UserDbType {
-    id?: string,
-    login: string,
-    password: string,
-    email: string,
-    createdAt: string,
-    emailConfirmation: emailInfo
+    id?:                    string,
+    login:                  string,
+    password:               string,
+    email:                  string,
+    createdAt:              string,
+    emailConfirmation:      emailInfo
 }
 
 export interface CommentatorInfo {
-    userId: string;
-    userLogin: string;
+    userId:         string;
+    userLogin:      string;
 }
 
 export interface CommentDbType {
-    id?: string;
-    content: string;
-    commentatorInfo: CommentatorInfo;
-    createdAt: string;
-    postId: string;
+    id?:                string;
+    content:            string;
+    commentatorInfo:    CommentatorInfo;
+    createdAt:          string;
+    postId:             string;
 }
 
 export interface RefreshTokenType {
-    userId: string;
-    refreshToken: string
+    userId:         string;
+    refreshToken:   string
 }
 
 export interface SessionCollection {
-    issuedAt: string; // exp refresh token
-    deviceId: string; // UUID
-    ip: string; // req.ip
-    lastActiveDate: string; //iat
-    deviceName: string; // user.agent
-    // refreshToken: string;
+    issuedAt:           string;           // exp refresh token
+    deviceId:           string;           // UUID
+    ip:                 string;                 // req.ip
+    lastActiveDate:     string;     // iat
+    deviceName:         string;         // user.agent
+    //TODO: мб надо убрать
+    refreshToken?:      string;
 }
