6d34692916a574d6df2ef6e7c20f3c2e785ce380
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-07 18:08:26 +0500

    Arrow Function with Implicit Return

--- src/utils/application/hash-service.ts
+++ src/utils/application/hash-service.ts
@@ -2,13 +2,10 @@ import bcrypt from 'bcrypt'
 
 export const hashService = {
     async _generateHash(password: string, saltRounds: number = 10): Promise<string> {
-        // генерируем соль
         const salt = await bcrypt.genSalt(saltRounds);
-        // генерируем хэш на основе пароля и созданной соли
         return  await bcrypt.hash(password, salt)
     },
     async comparePassword(password: string, hash: string): Promise<boolean> {
-        // сравниваем пароль с хэшом
         return await bcrypt.compare(password, hash);
     }
 }
