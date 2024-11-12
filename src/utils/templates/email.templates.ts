e41080e48ef6665102c81eaab3e98a2e8d19ecbc
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-12 19:05:43 +0500

    execute rename

--- src/utils/templates/email.templates.ts
+++ src/utils/templates/email.templates.ts
@@ -1,6 +1,6 @@
 export const emailTemplates = {
     registrationEmailTemplate(code: string) {
-        console.log('[emailTemplates] это jwt: ' + code);
+        console.log('[emailTemplates] это код email: ' + code);
         return `<h1>Thanks for your registration</h1>
         <p>To finish registration please follow the link below:
         <a href='https://localhost.com/confirm-email?code=${code}'>complete registration</a>
