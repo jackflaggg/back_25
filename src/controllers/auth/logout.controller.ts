22b32689f8be4dff01331f7519a5c18675f6c891
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-12 18:36:39 +0500

    execute rename

--- src/controllers/auth/logout.controller.ts
+++ src/controllers/auth/logout.controller.ts
@@ -2,6 +2,8 @@ import {Request, Response} from "express";
 import {HTTP_STATUSES} from "../../models/common/common.types";
 import {refreshTokenCollection} from "../../db/db";
 import {jwtService} from "../../utils/application/jwt.service";
+import {authService} from "../../domain/auth/auth.service";
+import {LoginErrorTwo} from "../../models/auth/ouput/auth.service.models";
 
 export const logoutController = async (req: Request, res: Response) => {
 
@@ -19,7 +21,13 @@ export const logoutController = async (req: Request, res: Response) => {
         res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION_401);
         return;
     }
-    await refreshTokenCollection.insertOne({userId, refreshToken});
+
+    const revokeRefreshToken = await authService.revokeRefreshToken(userId, refreshToken);
+    if (revokeRefreshToken instanceof LoginErrorTwo || revokeRefreshToken.data === null) {
+        console.log(`[authService] не получилось отозвать!`)
+        res.sendStatus(HTTP_STATUSES.NOT_AUTHORIZATION_401);
+        return;
+    }
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
     return;
 }
