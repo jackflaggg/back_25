a8005a7422999646e643b0f5c17d830f6580ac20
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-13 18:27:48 +0500

    execute rename

--- src/repositories/security-devices/security.devices.query.repository.ts
+++ src/repositories/security-devices/security.devices.query.repository.ts
@@ -1,13 +1,14 @@
 import {sessionCollection} from "../../db/db";
+import {outDeviceMapper} from "../../utils/mappers/device.mapper";
 
 export const securityDevicesQueryRepository = {
-    async getSessionToUserId(userId: string): Promise<any> {
+    async getSessionToUserId(userId: string) {
         try {
             const oneSession = await sessionCollection.findOne({userId});
             if (!oneSession) {
                 return null;
             }
-            return oneSession
+            return outDeviceMapper(oneSession);
         } catch (error: unknown) {
             return null
         }
--- src/utils/mappers/device.mapper.ts
+++ src/utils/mappers/device.mapper.ts
@@ -1,4 +1,6 @@
 import {InDeviceSession} from "../../models/devices/input/create.device.session.model";
+import {WithId} from "mongodb";
+import {SessionCollection} from "../../models/db/db.models";
 
 export const deviceMapper = (model: InDeviceSession) => {
     return {
@@ -9,5 +11,14 @@ export const deviceMapper = (model: InDeviceSession) => {
         lastActiveDate: model.lastActiveDate,
         deviceName: model.deviceName,
         refreshToken: model.refreshToken,
+    }
+}
+
+export const outDeviceMapper = (model: WithId<SessionCollection>) => {
+    return {
+        ip: model.ip,
+        title: model.deviceName,
+        lastActiveDate: model.lastActiveDate,
+        deviceId: model.deviceId,
     }
 }
