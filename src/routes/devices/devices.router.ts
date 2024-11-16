7823bae0ff2a9160f8aab51759f3e699af11fb52
jackflaggg <rasul.khamzinnn@gmail.com>
2024-11-16 23:37:56 +0500

    execute rename

--- src/routes/devices/devices.router.ts
+++ src/routes/devices/devices.router.ts
@@ -2,9 +2,10 @@ import {Router} from "express";
 import {getAllDevicesController} from "../../controllers/devices/get.all.active.sessions.controller";
 import {deleteSessionController} from "../../controllers/devices/delete.session.controller";
 import {deleteAllSessionsController} from "../../controllers/devices/delete.all.session.but.cur.controller";
+import {verifyTokenInCookieMiddleware} from "../../utils/middlewares/verify.token.in.cookie.middleware";
 
 export const devicesRouter: Router = Router();
 
-devicesRouter.get("/devices",getAllDevicesController)
-devicesRouter.delete("/devices", deleteAllSessionsController)
-devicesRouter.delete("/devices/:id", deleteSessionController)
+devicesRouter.get("/devices", verifyTokenInCookieMiddleware, getAllDevicesController)
+devicesRouter.delete("/devices", verifyTokenInCookieMiddleware, deleteAllSessionsController)
+devicesRouter.delete("/devices/:id", verifyTokenInCookieMiddleware, deleteSessionController)
